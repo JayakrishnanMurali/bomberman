b321f4b0d7dfc5728fce2b9f37db4cea7a3423f6
jayakrishnanm <jayakrishnan.m@neoito.com>
2025-03-31 11:21:51 +0530

    Initial Commit

---
+++ bomberman/bombermanApp.swift
@@ -0,0 +1,17 @@
+//
+//  bombermanApp.swift
+//  bomberman
+//
+//  Created by Jayakrishnan M on 31/03/25.
+//
+
+import SwiftUI
+
+@main
+struct bombermanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
